d561cf4c0978416eac3e1cb3b197304733629674
John (Jef) Fisher <Fishe545@gmail.com>
2021-07-30 17:32:22 +0800

    feat: called api and return formatted code

--- tvmaze.js
+++ tvmaze.js
@@ -2,7 +2,6 @@
  *     { id, name, summary, episodesUrl }
  */
 
-
 /** Search Shows
  *    - given a search term, search for tv shows that
  *      match that query.  The function is async show it
@@ -20,26 +19,41 @@
 async function searchShows(query) {
   // TODO: Make an ajax request to the searchShows api.  Remove
   // hard coded data.
-
-  return [
+  const params = {
+    params: {
+      q: query,
+    },
+  }
+  const res = await axios.get('https://api.tvmaze.com/search/shows', params)
+  const showData = res.data[0].show
+  const { id, name, summary, image } = showData
+  const info = [
     {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
-      image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
+      id,
+      name,
+      summary,
+      image: image.medium,
+    },
   ]
+  console.log(showData)
+  console.log(info)
+  // return [
+  //   {
+  //     id: 1767,
+  //     name: "The Bletchley Circle",
+  //     summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
+  //     image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+  //   }
+  // ]
 }
-
-
 
 /** Populate shows list:
  *     - given list of shows, add shows to DOM
  */
 
 function populateShows(shows) {
-  const $showsList = $("#shows-list");
-  $showsList.empty();
+  const $showsList = $('#shows-list')
+  $showsList.empty()
 
   for (let show of shows) {
     let $item = $(
@@ -51,31 +65,30 @@ function populateShows(shows) {
            </div>
          </div>
        </div>
-      `);
+      `,
+    )
 
-    $showsList.append($item);
+    $showsList.append($item)
   }
 }
-
 
 /** Handle search form submission:
  *    - hide episodes area
  *    - get list of matching shows and show in shows list
  */
 
-$("#search-form").on("submit", async function handleSearch (evt) {
-  evt.preventDefault();
+$('#search-form').on('submit', async function handleSearch(evt) {
+  evt.preventDefault()
 
-  let query = $("#search-query").val();
-  if (!query) return;
+  let query = $('#search-query').val()
+  if (!query) return
 
-  $("#episodes-area").hide();
+  $('#episodes-area').hide()
 
-  let shows = await searchShows(query);
+  let shows = await searchShows(query)
 
-  populateShows(shows);
-});
-
+  populateShows(shows)
+})
 
 /** Given a show ID, return list of episodes:
  *      { id, name, season, number }
@@ -85,6 +98,5 @@ async function getEpisodes(id) {
   // TODO: get episodes from tvmaze
   //       you can get this by making GET request to
   //       http://api.tvmaze.com/shows/SHOW-ID-HERE/episodes
-
   // TODO: return array-of-episode-info, as described in docstring above
 }

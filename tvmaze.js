6cb1732b66208a29cfea5ea52866e731cfb1d60d
John (Jef) Fisher <Fishe545@gmail.com>
2021-07-30 17:41:58 +0800

    fix: added logic for show images and removed hard coded input value in html

--- tvmaze.js
+++ tvmaze.js
@@ -35,16 +35,7 @@ async function searchShows(query) {
       image: image.medium,
     },
   ]
-  console.log(showData)
-  console.log(info)
-  // return [
-  //   {
-  //     id: 1767,
-  //     name: "The Bletchley Circle",
-  //     summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
-  //     image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-  //   }
-  // ]
+  return info
 }
 
 /** Populate shows list:
@@ -60,6 +51,9 @@ function populateShows(shows) {
       `<div class="col-md-6 col-lg-3 Show" data-show-id="${show.id}">
          <div class="card" data-show-id="${show.id}">
            <div class="card-body">
+           <img class="card-img-top" src=${
+             show.image ? show.image : 'https://tinyurl.com/tv-missing'
+           }>
              <h5 class="card-title">${show.name}</h5>
              <p class="card-text">${show.summary}</p>
            </div>
